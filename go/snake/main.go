e226128c6a2f4518bf06dd97113a8269f7d3d1a4
TheGoldKay <jhonnatanwfc@live.com>
2025-06-04 21:34:37 -0300

    player (square for now) && expand game struct

--- go/snake/main.go
+++ go/snake/main.go
@@ -7,7 +7,18 @@ import (
 	"image/color"
 )
 
-type Game struct{}
+type Player struct{
+	x, y float32
+	ln float32
+	color color.RGBA
+}
+
+type Game struct{
+	player Player
+	bg_color color.RGBA
+	win_w int 
+	win_h int
+}
 
 func (g *Game) Update() error {
 	// Close the game when the Escape key is pressed
@@ -19,7 +30,8 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// Fill the screen with green
-	vector.DrawFilledRect(screen, 0, 0, float32(screen.Bounds().Dx()), float32(screen.Bounds().Dy()), color.RGBA{G: 255, B: 100, A: 255}, true)
+	vector.DrawFilledRect(screen, 0, 0, float32(g.win_w), float32(g.win_h), g.bg_color, true)
+	vector.DrawFilledRect(screen, g.player.x, g.player.y, g.player.ln, g.player.ln, g.player.color, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -27,9 +39,22 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	window_width := 640
+	window_height := 480
+	ebiten.SetWindowSize(window_width, window_height)
 	ebiten.SetWindowTitle("Green Screen Game")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game := &Game{
+		player: Player{
+			x: float32(window_width / 2),
+			y: float32(window_height / 2),
+			ln: 50,
+			color: color.RGBA{G: 255, B: 220, A: 255},
+		},
+		bg_color: color.RGBA{G: 255, B: 100, A: 255},
+		win_w: window_width,
+		win_h: window_height,
+	}
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
