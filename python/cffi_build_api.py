b99da595937dcc6b65a0d2a548650c77f63f58e0
TheGoldKay <jhonnatanwfc@live.com>
2025-06-29 19:56:20 -0300

    pythonic

--- python/cffi_build_api.py
+++ python/cffi_build_api.py
@@ -1,13 +1,13 @@
 # build_api.py
 from cffi import FFI
-ffibuilder = FFI()
+ffi = FFI()
 
-ffibuilder.cdef("""
+ffi.cdef("""
     double sqrt(double x);
 """)
-ffibuilder.set_source("_mymath",  # generates _mymath* extension
+ffi.set_source("_mymath",  # generates _mymath* extension
     '#include <math.h>',
     libraries=['m'])
 
 if __name__ == "__main__":
-    ffibuilder.compile(verbose=True)
+    ffi.compile(verbose=True)
